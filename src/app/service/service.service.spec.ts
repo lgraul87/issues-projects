ef8b0c31e07a3226a2b9121914272448512d5be7
lgraul87 <lgraul87@gmail.com>
2022-01-29 18:41:58 +0100

    testing

--- src/app/service/service.service.spec.ts
+++ src/app/service/service.service.spec.ts
@@ -1,13 +1,25 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { ServiceService } from './service.service';
 
 describe('ServiceService', () => {
   let service: ServiceService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ServiceService],
+    });
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(ServiceService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
