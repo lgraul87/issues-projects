c9ce716b038424d29b74d0618e6ffca0cb606a57
lgraul87 <lgraul87@gmail.com>
2022-01-30 02:26:17 +0100

    added test

--- src/app/service/issue-test.spec.ts
+++ src/app/service/issue-test.spec.ts
@@ -7,23 +7,32 @@ import { IssuesService } from './issue-service';
 
 describe('issues service', () => {
   let service: IssuesService;
-  let httpTestingController: HttpTestingController;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [IssuesService],
     });
-    httpTestingController = TestBed.inject(HttpTestingController);
+    httpMock = TestBed.inject(HttpTestingController);
     service = TestBed.inject(IssuesService);
   });
 
   afterEach(() => {
-    httpTestingController.verify();
+    httpMock.verify();
   });
 
   it('should be created', () => {
-    expect(service.getIssues()).toBeTruthy();
+    expect(service).toBeTruthy();
+  });
+
+  it('should return issue list and by GET method', () => {
+    const issueMock: any[] = [{ title: 'issue-1' }, { title: 'issue-2' }];
+    service.getIssues().subscribe((issues) => {
+      expect(issues.length).toEqual(issueMock.length);
+    });
+    const req = httpMock.expectOne(`${service['url']}`);
+    req.flush(issueMock);
+    expect(req.request.method).toBe('GET');
   });
 });
-
